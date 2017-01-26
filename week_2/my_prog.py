9a89a52014c857ca10b4ca0a2c4ecdfeef3a6494
Juan P. Cerezo <juampe@cerezo.org>
2017-01-26 16:40:08 +0100

    ending lesson 2

---
+++ week_2/my_prog.py
@@ -0,0 +1,33 @@
+
+from my import print_hello
+#import my
+
+print_hello()
+
+
+'''
+>>> import sys
+>>> from pprint import pprint
+>>> pprint(sys.path)
+['',
+ '/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python27.zip',
+ '/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7',
+ '/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-darwin',
+ '/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-mac',
+ '/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-mac/lib-scriptpackages',
+ '/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk',
+ '/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-old',
+ '/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/readline',
+ '/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-dynload',
+ '/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages']
+ '''
+
+ '''
+ export PYTHONPATH='/Users/jpc/GIT/ANS_PY_course/week_2/subdir'
+
+ or relocating libraries on usr/lib/python2.7/site-packages libs
+or 
+    ~/GIT/ANS_PY_course/week_2/lib/python2.7/site-packages
+
+'''
+
