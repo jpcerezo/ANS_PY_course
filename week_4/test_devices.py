ad19ed16524d2da042182d16ffeaab6bfa0c11db
Juan P. Cerezo <juampe@cerezo.org>
2017-03-02 21:08:21 +0100

    week 4 code

---
+++ week_4/test_devices.py
@@ -0,0 +1,24 @@
+
+password=''
+cisco1 = {
+	'device_type': 'cisco_ios',
+	'ip': '192.168.1.240',
+	'username': 'juampe',
+	'password': password,
+}
+
+cisco2 = {
+	'device_type': 'cisco_ios',
+	'ip': '192.168.1.241',
+	'username': 'juampe',
+	'password': password,
+	'port': 22,
+}
+
+vsrx1 = {
+	'device_type': 'juniper',
+	'ip': '192.168.1.171',
+	'username': 'juampe',
+	'password': password,
+	'secret': '',
+}
